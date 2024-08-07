a86bd3b0d15abbd7e9a0c2c1c5566876c7916463
Teju22907 <kalluritejaswini334@gmail.com>
2024-08-07 15:41:43 +1000

    Add files via upload

---
+++ footy tips.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome()
+
+
+driver.find_element(By.XPATH("//button[@class='StyledButton-sc-17v63mz-0 dRwpqZ']")).click()
+
+time.sleep(5)
+
+driver.find_element(By.XPATH("//input[@id='InputIdentityFlowValue']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//input[@id='InputPassword']"))
+driver.find_element(By.XPATH("//button[@id='BtnSubmit']"))
+driver.find_element(By.XPATH("//button[@class='StyledButton-sc-17v63mz-0 dRwpqZ']"))
+driver.find_element(By.XPATH("//button[@class='StyledButton-sc-17v63mz-0 dRwpqZ']"))
+driver.find_element(By.XPATH("//p[normalize-space()='Home']"))
+driver.find_element(By.XPATH("//p[normalize-space()='Home']"))
+driver.find_element(By.XPATH("//p[normalize-space()='Settings']"))
+driver.find_element(By.XPATH("//p[normalize-space()='Settings']"))
+driver.find_element(By.XPATH("//p[normalize-space()='Settings']"))
+driver.find_element(By.XPATH("//div[@class='NavRight-sc-o9g8qg-6 fIWbrK']//li[1]"))
+
