8a5e72b21affe0105e94dc81e06891d6c43cc46d
Teju22907 <kalluritejaswini334@gmail.com>
2024-08-08 10:42:50 +1000

    Update goal.py

--- goal.py
+++ goal.py
@@ -9,25 +9,25 @@ driver = webdriver.Chrome()
 driver.get('https://www.goal.com/en-au')
 time.sleep(5)
 
-# def account_creation():
-#     driver.find_element(By.XPATH,'/html/body/div[1]/header/nav/div/a[3]').click()
-#     time.sleep(5)
-#     driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/p/a/span').click()
-#     time.sleep(5)
-#     driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/div[2]/button[1]').click()
-#     time.sleep(5)
-#     driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/div[1]/label/input').send_keys('kalluritejaswini334@gmail.com')
-#     time.sleep(5)
+def account_creation():
+    driver.find_element(By.XPATH,'/html/body/div[1]/header/nav/div/a[3]').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/p/a/span').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/div[2]/button[1]').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/div[1]/label/input').send_keys('kalluritejaswini334@gmail.com')
+    time.sleep(5)
 
-#     driver.find_element(By.NAME,'password').send_keys('VamsiTeju@26')
-#     time.sleep(5)
-#     driver.find_element(By.NAME,'repeatedPassword').send_keys('VamsiTeju@26')
-#     driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/label/div').click()
-#     time.sleep(5)
-#     driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/button').click()
-#     time.sleep(5)
+    driver.find_element(By.NAME,'password').send_keys('VamsiTeju@26')
+    time.sleep(5)
+    driver.find_element(By.NAME,'repeatedPassword').send_keys('VamsiTeju@26')
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/label/div').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div[1]/div/form/button').click()
+    time.sleep(5)
 
-# account_creation()
+account_creation()
 
 def loginlogout():
     driver.find_element(By.XPATH,'/html/body/div[1]/header/nav/div/a[3]').click()
@@ -39,8 +39,8 @@ def loginlogout():
     driver.find_element(By.XPATH,'/html/body/div/div/div/div/div/div[1]/div/form/button').click()
     time.sleep(5)
 
-    # driver.find_element(By.XPATH,'/html/body/div/div/div/div/div/div[1]/div/form/button').click()
-    # time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div/div/div/div/div/div[1]/div/form/button').click()
+    time.sleep(5)
 
     driver.find_element(By.XPATH,'/html/body/div[1]/header/nav/div/div/a').click()
     time.sleep(5)
